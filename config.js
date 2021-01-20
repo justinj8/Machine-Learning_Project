ddd5facc86e95e8b079e367ef84ba31b25e6fc9a
Farah Awad <farahtawad@gmail.com>
2021-01-20 17:31:53 -0500

    prepped data for machine learning model to take in

--- config.js
+++
@@ -1,4 +0,0 @@
-var config = {
-tone_key = "SbOsTBTDmyHBLBTGVEjlZjKXgUHlpBk6CcnvWSHUH4NN"
-tone_url = "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/52ba257b-93de-4159-a79c-2d6b61f1a3b1"
-}
